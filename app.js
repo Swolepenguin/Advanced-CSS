29ccabd5c3a8a41e19e998c26c1ac43cf1875bac
swolepenguin <ashtonm223@gmail.com>
2020-10-27 12:01:18 -0400

    adding some style practice

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const body = document.querySelector('body')
+container.addEventListener('mouseover'), () => {
+    const newElement= createNewElement('p,this is a new Element') 
+    newElement.appendChild('body')
+}
